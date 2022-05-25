ae1e490697d5e3c17afc40b60aa9d26a51626a4d
Rudranil Sarkar <rudranil.sarkar1.com>
2022-05-25 16:23:47 +0530

    new frontend added - React

--- Choose_Your_Stack/CYS_backend/CYS_codebase/app.py
+++ Choose_Your_Stack/CYS_backend/CYS_codebase/app.py
@@ -45,4 +45,4 @@ def download_Object():
 if __name__ == '__main__':
 	# run() method of Flask class runs the application
 	# on the local development server.
-	app.run(host='0.0.0.0', port=5002 ,debug = True)
+	app.run(host='0.0.0.0')
